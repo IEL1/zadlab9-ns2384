037472200a2892e055b5fa3c56d6a4e8962f96bc
dsadas <dsadas@dadasd.com>
2018-05-07 17:56:16 +0200

    lab 9

--- OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package pl.edu.ur.oopl9;
-
+import java.util.Random;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Iterator;
 /**
  *
  * @author maarchyl
@@ -14,8 +18,47 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) { 
+     
+        //Zad 1
+        
+     /* int[] tab = new int[100];
+        System.out.println("Losowanie:");
+        Random r = new Random();
+            
+            for(int i=0;i<100;i++)
+            {
+                    tab[i]=r.nextInt();
+                    System.out.println(tab[i]);
+            }
+            
+        System.out.println("Sortowanie: ");
+        Arrays.sort(tab);
+        for(int i=0;i<100;i++)
+        {
+            System.out.println(tab[i]);
+        }
+        }
+       */
+   
+    //Zad 2
+     int a;
+     
+     Random r = new Random();
+     TreeSet x = new TreeSet();
+     for(int i=0; i<100; i++)
+     {
+         a = r.nextInt();
+         x.add(a);
+     }
+     Iterator it = x.iterator();
+     while(it.hasNext())
+     {
+         System.out.println(it.next());
+     }
     }
+    
+    
+           
     
 }
